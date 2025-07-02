f7c23f70b2605bdda03cfcf7f26d618f5e57ece0
Volodymyr <volodymyr.demkovskyi@nymcard.com>
2025-07-03 00:24:22 +0200

    Small refactoring LoginView

--- Testio/Scenes/Login/LoginView.swift
+++ Testio/Scenes/Login/LoginView.swift
@@ -14,56 +14,18 @@ struct LoginView: View {
     var body: some View {
         ZStack {
             backgroundView
-
-            if viewModel.authenticationStatus == .loading {
-                loadListView
-            } else {
-                loginFormView
-            }
+            contentView
         }
         .ignoresSafeArea()
-        .alert(isPresented: Binding(
-            get: { viewModel.alertInfo != nil },
-            set: { _ in viewModel.alertInfo = nil }
-        )) {
-            if let alertInfo = viewModel.alertInfo {
-                if alertInfo.type == .withActions {
-                    return Alert(
-                        title: Text(alertInfo.title),
-                        message: Text(alertInfo.message),
-                        primaryButton: .default(Text(AppConfigurator.Strings.yes)) {
-                            Task {
-                                viewModel.authenticationTrigger.send(.saveBiometricData)
-                            }
-                        },
-                        secondaryButton: .cancel(Text(AppConfigurator.Strings.no)) {
-                            Task {
-                                viewModel.authenticationTrigger.send(.saveToken)
-                            }
-                        }
-                    )
-                } else {
-                    return Alert(
-                        title: Text(alertInfo.title),
-                        message: Text(alertInfo.message),
-                        dismissButton: .default(Text(AppConfigurator.Strings.ok))
-                    )
-                }
-            }
-            return Alert(
-                title: Text(AppConfigurator.Strings.errorTitle),
-                message: Text(AppConfigurator.Strings.anUnexpectedErrorOccurred),
-                dismissButton: .default(Text(AppConfigurator.Strings.ok))
-            )
-        }
+        .alert(isPresented: alertBinding, content: alertContent)
         .task {
             viewModel.authenticationTrigger.send(.checkAuthenticationType)
         }
     }
 }
 
-// MARK: - View Components
-fileprivate extension LoginView {
+// MARK: - Subviews
+private extension LoginView {
     var backgroundView: some View {
         VStack {
             Spacer()
@@ -73,7 +35,17 @@ fileprivate extension LoginView {
         }
     }
 
-    var loadListView: some View {
+    @ViewBuilder
+    var contentView: some View {
+        switch viewModel.authenticationStatus {
+        case .loading:
+            loadingView
+        default:
+            loginFormView
+        }
+    }
+
+    var loadingView: some View {
         ProgressView(AppConfigurator.Strings.loading)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(backgroundView)
@@ -83,12 +55,52 @@ fileprivate extension LoginView {
         VStack(spacing: AppConfigurator.Sizes.loginSpacing) {
             Spacer()
             AnimatedLogo()
-
             LoginFormView(viewModel: viewModel)
             Spacer()
         }
         .padding(AppConfigurator.Sizes.padding_32)
         .adjustForKeyboard()
+    }
+}
+
+// MARK: - Alerts
+private extension LoginView {
+    var alertBinding: Binding<Bool> {
+        Binding(
+            get: { viewModel.alertInfo != nil },
+            set: { _ in viewModel.alertInfo = nil }
+        )
+    }
+
+    
+    func alertContent() -> Alert {
+        guard let alertInfo = viewModel.alertInfo else {
+            return Alert(
+                title: Text(AppConfigurator.Strings.errorTitle),
+                message: Text(AppConfigurator.Strings.anUnexpectedErrorOccurred),
+                dismissButton: .default(Text(AppConfigurator.Strings.ok))
+            )
+        }
+
+        switch alertInfo.type {
+        case .withActions:
+            return Alert(
+                title: Text(alertInfo.title),
+                message: Text(alertInfo.message),
+                primaryButton: .default(Text(AppConfigurator.Strings.yes)) {
+                    Task { viewModel.authenticationTrigger.send(.saveBiometricData) }
+                },
+                secondaryButton: .cancel(Text(AppConfigurator.Strings.no)) {
+                    Task { viewModel.authenticationTrigger.send(.saveToken) }
+                }
+            )
+        case .simple:
+            return Alert(
+                title: Text(alertInfo.title),
+                message: Text(alertInfo.message),
+                dismissButton: .default(Text(AppConfigurator.Strings.ok))
+            )
+        }
     }
 }
 
